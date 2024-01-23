516024a079de49bef4b176cbff38c468f4a56645
Juan Guerra <juan.guerra@yale.edu>
2024-01-23 15:22:18 -0500

    #andmarla

--- scripts/get_ads.py
+++ scripts/get_ads.py
@@ -4,14 +4,14 @@ import yaml
 import glob
 import numpy as np
 
-def make_bib(authors, outfile="../_includes/most_recent_all.bib"):
+def make_bib(authors, outfile="scripts/most_recent_all.bib"):
 
     with open(outfile, "w+") as out:
         for author in authors:
             # Search for papers by the author, sorted by publication date (most recent first)
             papers = list(
                 ads.SearchQuery(
-                    q=f'author:"{author}"',
+                    q=f'author:"{author}" author: "Geha, Marla"',
                     fl=[
                         "citation_count",
                         "abbr",
@@ -24,7 +24,7 @@ def make_bib(authors, outfile="../_includes/most_recent_all.bib"):
             # Check if there are any papers for the author
             if papers:
                 # Get the most recent paper (first paper in the list)
-                most_recent_paper = papers[0]
+                most_recent_paper = papers[0:np.min(3,len(papers))]
                 
                 # Export the BibTeX entry for the most recent paper
                 bibquery = ads.ExportQuery(most_recent_paper.bibcode)
@@ -72,7 +72,7 @@ def main():
     collections = ['_pi','_postdocs','_grads','_undergrads']
     names = np.concatenate([get_names_from_collections(folder) for folder in collections ])
     names = [name.split(' ') for name in names]
-    names = ['^'+name[-1]+', '+name[0] for name in names]
+    names = [name[-1]+', '+name[0] for name in names]
     sorted_names = sorted(names)
     make_bib(sorted_names,outfile='scripts/most_recent_all.bib')
 
